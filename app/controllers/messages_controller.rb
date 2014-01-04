c63fca6c279a2731ae9619af772d3a725f76dca3
Ryan <a2306742@gmail.com>
2014-01-04 13:17:22 +0800

    add avatar to user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,26 @@
 class MessagesController < ApplicationController
 	before_action :set_message, only: [ :show, :edit , :update , :destroy]
+	before_action :correct_user, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+
 	
 
 	def index
-		@messages = Message.all
+		@messages = Message.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
-		@message = Message.new
+		#@message = Message.new
+		@message = current_user.messages.build
 	end
 
 	def create
 		
-		@message = Message.new(message_params)
+		#@message = Message.new(message_params)
+		@message = current_user.messages.build(message_params)
 
 		if(@message.save)
 			redirect_to message_path(@message)
@@ -50,10 +55,15 @@ class MessagesController < ApplicationController
 			@message = Message.find(params[:id])
 		end
 
+				#防止別結透過網址入侵，修改別人的message
+		def correct_user		
+      	@message = current_user.messages.find_by_id(params[:id])
+      	redirect_to messages_path, notice: "Not authorized to edit this message" if @message.nil?
+    end
+
 		def message_params
 			params.require(:message).permit(:name , :message)
 		end
-
 
 
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ActiveRecord::Base
 
+	
+	belongs_to :user
 	validates :message, presence: true
 	validates_presence_of :name
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
 end
